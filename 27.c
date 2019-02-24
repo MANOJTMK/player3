940fce2039f07730ce9d311e42198d33b1882aae
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-24 21:50:59 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+	char str[20];
+  clrscr();
+	scanf("%s",str);
+	for(int i=0;str[i]!='\0';i++)
+	{
+		if(str[i]>96)
+		{
+			str[i]=str[i]-32;
+		}
+		else if(str[i]<91)
+		{
+			str[i]=str[i]+32;
+		}
+	}
+	printf("%s",str);
+		
+		
+		return 0;
+}
