c599a2a5d15d37025c51fdc563206f7ecea5208c
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-24 21:24:19 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,k,i,g,a,b,r;
+   cirscr();
+   scanf("%d%d",&n,&k);
+   a=n;
+   b=k;
+   while(b)
+   {
+       r=a%b;
+       a=b;
+       b=r;
+   }
+   g=a;
+   printf("%d",g);
+    return 0;
+}
